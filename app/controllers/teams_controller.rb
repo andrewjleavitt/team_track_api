777c114a57edd59589ef99e594f9adcab615f5b9
Andy Leavitt <andyleavitt@gmail.com>
2017-12-06 09:26:05 -0700

    initial commit

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,17 @@
+class TeamsController < ApplicationController
+  def index
+    @teams = Team.all
+    render json: @teams
+  end
+
+  def create
+    team = Team.create(team_params)
+    render json: team if team.persisted?
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name)
+  end
+end
