085ae0fad74d10b29ebabae428f9c817b7c8bf9d
Andy Leavitt <andyleavitt@gmail.com>
2017-12-12 23:16:29 -0700

    Members for teams can be created

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -4,4 +4,15 @@ class MembersController < ApplicationController
     members = Member.for_team(params[:team_id])
     render json: members
   end
+
+  def create
+    member = Member.create({name: member_params[:name], team_id: params[:team_id]})
+    render json: member if member.persisted?
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name)
+  end
 end
